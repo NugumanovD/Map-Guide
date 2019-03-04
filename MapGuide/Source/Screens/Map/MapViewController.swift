ba2b716a035742fd254d82b636c34c569599fb5a
Дмитрий Нугуманов <nugumanov6336@gmail.com>
2019-03-04 22:36:59 +0200

    jnlkjsdnf

--- MapGuide/Source/Screens/Map/MapViewController.swift
+++ MapGuide/Source/Screens/Map/MapViewController.swift
@@ -21,7 +21,7 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBarItem.tag = TabBarItemTag.navigationItem.rawValue
+        
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
