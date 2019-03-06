3561f24e2dd8440f1c0eee13aa2a37d42eccfeb7
Дмитрий Нугуманов <nugumanov6336@gmail.com>
2019-03-06 20:04:56 +0200

    test

--- MapGuide/Source/Restaurants.swift
+++ MapGuide/Source/Restaurants.swift
@@ -12,33 +12,6 @@ import RealmSwift
 class Restaurants {
     
     
-    func loadStateData() {
         
-//        let pektoral = StateModel()
-//        pektoral.name = "Пектораль"
-//        pektoral.latitude = 47.8171141648544
-//        pektoral.longitude = 35.088985562324524
-//
-//        let horticaHotel = StateModel()
-//        horticaHotel.name = "Хортица"
-//        horticaHotel.latitude = 47.8205721644365
-//        horticaHotel.longitude = 35.09705364704132
-//
-//        let sagaidachniyHotel = StateModel()
-//        sagaidachniyHotel.name = "Сагайдачный"
-//        sagaidachniyHotel.latitude = 47.824952
-//        sagaidachniyHotel.longitude = 35.090359
-//
-//        let restsArray = [pektoral, horticaHotel, sagaidachniyHotel]
-//
-//        let realmInstance = try! Realm()
-//
-//        try! realmInstance.write {
-//            for rest in restsArray {
-//                realmInstance.add(rest)
-//            }
-//        }
-    }
-    
     
 }
